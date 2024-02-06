9816ccdd36d0aaf1989aa34da9f29d870f46b272
unknown <christopherbloodsworth@gmail.com>
2024-02-06 07:59:28 -0500

    Finished 2.2

--- ruby/2.1.rb
+++ ruby/2.1.rb
@@ -2,15 +2,10 @@
 arr = []
 (0...16).each { |i| arr[i] = i }
 
-# Print four numbers at a time, only using each
-def newline_char(i)
-
-end
-
 # please god tell me there's a better way to do this 😭
-arr.each{ |i| print "#{arr[i]} #{(i+1) % 4 == 0 ? "\n" : ""}"}
+arr.each { |i| print "#{arr[i]} #{((i + 1) % 4).zero? ? "\n" : ''}" }
 
 puts '--------------------'
 
 # Print four numbers at a time, using each_slice
-arr.each_slice(4).each{|x| puts x.join(' ')}
+arr.each_slice(4).each { |x| puts x.join(' ') }
